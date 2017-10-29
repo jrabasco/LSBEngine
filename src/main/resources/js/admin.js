39887e343d617c9281fe13f2c7b45fa3f26a1d7e
Jeremy Rabasco <rabasco.jeremy@gmail.com>
2017-10-29 18:35:48 +0000

    Add anchor link, fixes #36

--- src/main/resources/js/admin.js
+++ src/main/resources/js/admin.js
@@ -1,4 +1,22 @@
 var waiting = false;
+showdown.setOption('headerLevelStart', 2);
+//Extension
+showdown.extension('header-anchors', function() {
+
+	var ancTpl = '$1<a id="user-content-$3" class="anchor" href="#$3" aria-hidden="true"><svg aria-hidden="true" class="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>$4';
+
+  return [{
+    type: 'html',
+    regex: /(<h([1-3]) id="([^"]+?)">)(.*<\/h\2>)/g,
+    replace: ancTpl
+  }];
+});
+
+function getConverter() {
+	return new showdown.Converter({
+		  extensions: ['header-anchors']
+	});
+}
 
 function doLogout() {
     $.ajax({
@@ -210,7 +228,7 @@ function updateAbout() {
         form.hide();
         loader.show();
 
-        var converter = new showdown.Converter();
+        var converter = getConverter();
         var introMarkdown = form[0].intromarkdown.value;
         var introHtml = converter.makeHtml(introMarkdown);
 
@@ -273,7 +291,7 @@ function updatePost(add) {
         var title = form[0].title.value;
         var summary = form[0].summary.value;
 
-        var converter = new showdown.Converter();
+        var converter = getConverter();
         var contentMarkdown = form[0].contentmarkdown.value;
         var contentHtml = converter.makeHtml(contentMarkdown);
         //Assures consistency between firefox and chrome
@@ -396,7 +414,7 @@ function fillPostPreview() {
     var title = form[0].title.value;
     var previewPost = $('.preview > .doc');
 
-    var converter = new showdown.Converter();
+    var converter = getConverter();
     var contentMarkdown = form[0].contentmarkdown.value;
     var contentHtml = converter.makeHtml(contentMarkdown);
 
@@ -412,7 +430,7 @@ function fillAboutMePreview() {
     var previewIntro = $('.preview > #introduction');
     var previewResume = $('.preview > #resume');
 
-    var converter = new showdown.Converter();
+    var converter = getConverter();
     var introMarkdown = form[0].intromarkdown.value;
     var introHtml = converter.makeHtml(introMarkdown);
 
